7af1a59875771f75247f4d15000083042a41c91c
Lior Levy <liorlevy@cs.washington.edu>
2020-01-21 20:42:00 -0800

    copied from local 1

---
+++ testreact/src/index.js
@@ -0,0 +1,173 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import './index.css';
+
+
+
+
+
+// const ExampleToast = ({ children }) => {
+//   const [show, toggleShow] = useState(true);
+
+//   return (
+//     <>
+//       {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+//       <Toast show={show} onClose={() => toggleShow(false)}>
+//         <Toast.Header>
+//           <strong className="mr-auto">React-Bootstrap</strong>
+//         </Toast.Header>
+//         <Toast.Body>{children}</Toast.Body>
+//       </Toast>
+//     </>
+//   );
+// };
+
+// const App = () => (
+//   <Container className="p-1">
+//     <Jumbotron>
+//       <h1 className="header">Welcome To React-Bootstrap</h1>
+//       <ExampleToast className="toast">
+//         We now have Toasts
+//         <span role="img" aria-label="tada">
+//           🎉
+//         </span>
+//       </ExampleToast>
+//     </Jumbotron>
+//   </Container>
+// );
+
+
+
+  function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+
+    );
+  }
+  
+  class Board extends React.Component {
+    
+    constructor (props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsSet: true,
+        };
+
+
+    }  
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+          }
+        squares[i] = this.state.xIsSet ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsSet: !this.state.xIsSet,
+        
+        });
+      }
+    
+      renderSquare(i) {
+         return (
+          <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+          />
+        );
+      }
+    
+  
+    render() {
+     
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+        status = 'Winner: ' + winner;
+        } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+           
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  
+  // ========================================
+  
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
+  
